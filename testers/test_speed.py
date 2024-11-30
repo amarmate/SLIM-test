dd28da82ed5b54b5bb673d0ef8daf92b9fd1ce8f
Mateus Amaral <mbaptistaamaral@gmail.com>
2024-11-30 23:11:38 +0000

    update

--- testers/test_speed.py
+++ testers/test_speed.py
@@ -5,6 +5,9 @@ from multiprocessing import Pool, cpu_count
 
 datasets = [globals()[i] for i in globals() if 'load' in i][2:]
 
+# Repeat datasets 10 times 
+datasets = datasets * 10
+
 args = {
     'p_inflate': 0.3,
     'max_depth': 21,
@@ -21,7 +24,7 @@ def process_dataset(dataset):
     rm, ma, nrmse, r2, mae, std_rmse, time, train, test, size = test_slim(
         X=X, y=y, args_dict=args, dataset_name='test',
         ms_lower=0, ms_upper=1, n_elites=1,
-        iterations=15, scale=True, algorithm='SLIM*SIG1',
+        iterations=4, scale=True, algorithm='SLIM*SIG1',
         verbose=0, p_train=0.7, show_progress=True,
     )
     rm = np.array(rm)
